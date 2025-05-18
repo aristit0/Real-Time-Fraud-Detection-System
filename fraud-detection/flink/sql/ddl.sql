754bf8fa1b8bd21f6f1670e21099564c18ef4237
aristit0 <aristitosugihharto@gmail.com>
2025-05-18 13:36:53 +0700

    update

--- fraud-detection/flink/sql/ddl.sql
+++ fraud-detection/flink/sql/ddl.sql
@@ -1,15 +1,142 @@
-CREATE TABLE transactions (
-  transaction_id STRING,
-  account_id STRING,
-  amount STRING,
-  transaction_type STRING,
-  location STRING,
-  device_id STRING,
-  timestamp BIGINT,
-  op STRING
+CREATE TABLE `ssb`.`Real-Time-Fraud-Detection-System`.`fraud_transaction_flattened` (
+  `payload` ROW<
+    `after` ROW<
+      `transaction_id` STRING,
+      `account_id` STRING,
+      `amount` STRING,
+      `transaction_type` STRING,
+      `location` STRING,
+      `device_id` STRING,
+      `timestamp` BIGINT
+    >
+  >
+)
+WITH (
+  'properties.ssl.truststore.password' = 'Admin123',
+  'properties.auto.offset.reset' = 'earliest',
+  'properties.ssl.keystore.password' = 'Admin123',
+  'format' = 'json',
+  'properties.security.protocol' = 'SASL_SSL',
+  'scan.startup.mode' = 'latest-offset',
+  'properties.bootstrap.servers' = 'cdpm1.cloudeka.ai:9093,cdpm2.cloudeka.ai:9093,cdpm3.cloudeka.ai:9093',
+  'properties.ssl.keystore.location' = '/opt/kafka-conf/cm-auto-host_keystore.jks',
+  'connector' = 'kafka',
+  'properties.request.timeout.ms' = '120000',
+  'properties.ssl.truststore.location' = '/opt/kafka-conf/cm-auto-global_truststore.jks',
+  'properties.transaction.timeout.ms' = '900000',
+  'topic' = 'transaction.transaction.fraud_transaction_data',
+  'properties.group.id' = 'fraud_transaction_customer_group',
+  'properties.sasl.kerberos.service.name' = 'kafka'
+);
+
+CREATE TABLE `ssb`.`Real-Time-Fraud-Detection-System`.`fraud_transaction_flattened` (
+  `payload` ROW<`after` ROW<`transaction_id` VARCHAR(2147483647), `account_id` VARCHAR(2147483647), `amount` VARCHAR(2147483647), `transaction_type` VARCHAR(2147483647), `location` VARCHAR(2147483647), `device_id` VARCHAR(2147483647), `timestamp` BIGINT>>
+) WITH (
+  'properties.ssl.truststore.password' = 'Admin123',
+  'properties.auto.offset.reset' = 'earliest',
+  'properties.ssl.keystore.password' = 'Admin123',
+  'format' = 'json',
+  'properties.security.protocol' = 'SASL_SSL',
+  'scan.startup.mode' = 'group-offsets',
+  'properties.bootstrap.servers' = 'cdpm1.cloudeka.ai:9093,cdpm2.cloudeka.ai:9093,cdpm3.cloudeka.ai:9093',
+  'properties.ssl.keystore.location' = '/opt/kafka-conf/cm-auto-host_keystore.jks',
+  'connector' = 'kafka',
+  'properties.request.timeout.ms' = '120000',
+  'properties.ssl.truststore.location' = '/opt/kafka-conf/cm-auto-global_truststore.jks',
+  'properties.transaction.timeout.ms' = '900000',
+  'topic' = 'transaction.transaction.fraud_transaction_data',
+  'properties.group.id' = 'fraud_transaction_customer_group',
+  'properties.sasl.kerberos.service.name' = 'kafka'
+)
+
+
+
+
+
+#### kafka sink
+DROP TABLE IF EXISTS `ssb`.`Real-Time-Fraud-Detection-System`.`fraud_alerts`;
+
+CREATE TABLE `ssb`.`Real-Time-Fraud-Detection-System`.`fraud_alerts` (
+  `transaction_id` STRING,
+  `account_id` STRING,
+  `amount` STRING,
+  `fraud_type` STRING,
+  `flagged_time` TIMESTAMP  -- ✅ No (3), no WITH TIME ZONE
 ) WITH (
   'connector' = 'kafka',
-  'topic' = 'transaction_data.transaction.transaction_data',
-  'format' = 'avro',
-  'scan.startup.mode' = 'latest-offset'
+  'topic' = 'fraud_alerts',
+  'format' = 'json',
+  'properties.bootstrap.servers' = 'cdpm1.cloudeka.ai:9093,cdpm2.cloudeka.ai:9093,cdpm3.cloudeka.ai:9093',
+  'properties.sasl.jaas.config' = 'org.apache.kafka.common.security.plain.PlainLoginModule required username="cmluser" password="hwj5GpM8rVgy";',
+  'properties.sasl.mechanism' = 'PLAIN',
+  'properties.security.protocol' = 'SASL_SSL',
+  'properties.group.id' = 'fraud-alerts-consumer-group',
+  'scan.startup.mode' = 'latest-offset',
+  'properties.sasl.kerberos.service.name' = 'kafka'
 );
+
+
+CREATE TABLE `ssb`.`Real-Time-Fraud-Detection-System`.`fraud_alerts` (
+  `transaction_id` VARCHAR(2147483647),
+  `account_id` VARCHAR(2147483647),
+  `amount` VARCHAR(2147483647),
+  `fraud_type` VARCHAR(2147483647),
+  `flagged_time` TIMESTAMP(6)
+) WITH (
+  'properties.bootstrap.servers' = 'cdpm1.cloudeka.ai:9093,cdpm2.cloudeka.ai:9093,cdpm3.cloudeka.ai:9093',
+  'properties.sasl.jaas.config' = 'org.apache.kafka.common.security.plain.PlainLoginModule required username="cmluser" password="hwj5GpM8rVgy";',
+  'connector' = 'kafka',
+  'properties.sasl.mechanism' = 'PLAIN',
+  'format' = 'json',
+  'properties.security.protocol' = 'SASL_SSL',
+  'topic' = 'fraud_alerts',
+  'properties.group.id' = 'fraud-alerts-consumer-group',
+  'scan.startup.mode' = 'earliest-offset',
+  'properties.sasl.kerberos.service.name' = 'kafka'
+)
+
+
+
+
+#iceberg sink
+
+CREATE TABLE `fraud_alerts` (
+    transaction_id STRING,
+    account_id STRING,
+    amount STRING,
+    fraud_type STRING,
+    flagged_time TIMESTAMP
+) WITH (
+  'connector' = 'iceberg',
+  'catalog-name' = 'hive',
+  'catalog-type' = 'hive',
+  'catalog-database' = 'datamart',
+  'catalog-table' = 'fraud_alerts',
+  'engine.hive.enabled' = 'true',
+  'ssb-hive-catalog' = 'ssb_hive_catalog'
+);
+
+
+
+
+
+result
+CREATE EXTERNAL TABLE datamart.fraud_alerts
+ ( transaction_id STRING NULL, account_id STRING NULL, amount STRING NULL, fraud_type STRING NULL, flagged_time TIMESTAMP NULL )
+  STORED AS ICEBERG LOCATION 'hdfs://cloudeka-hdfs/warehouse/tablespace/external/hive/datamart.db/fraud_alerts'
+  TBLPROPERTIES ('catalog-database'='datamart', 'catalog-name'='fraud_alerts', 'catalog-table'='fraud_alerts', 
+  'catalog-type'='hive', 'connector'='iceberg', 'engine.hive.enabled'='true', 'properties.group.id'='fraud-alerts-iceberg',
+   'scan.startup.mode'='earliest-offset', 'sink.commit-policy'='success-file', 'sink.parallelism'='1', 'table_type'='ICEBERG', 'write.upsert.enabled'='false')
+
+
+/etc/hadoop/conf/hdfs-site.xml,
+/etc/hadoop/conf/mapred-site.xml,
+/etc/hadoop/conf/ssl-client.xml,
+/etc/hadoop/conf/core-site.xml,
+/etc/hadoop/conf/yarn-site.xml,
+/etc/hadoop/conf/ozone-site.xml
+
+/opt/kafka-conf/cm-auto-host_keystore.jks
+
+
+/opt/kafka-conf/cm-auto-global_truststore.jks
--- fraud-detection/flink/sql/detection_query.sql
+++ fraud-detection/flink/sql/detection_query.sql
@@ -1,24 +1,23 @@
--- High amount detection
+INSERT INTO `ssb`.`Real-Time-Fraud-Detection-System`.`fraud_alerts`
 SELECT
   transaction_id,
   account_id,
   amount,
   'HIGH_VALUE_TXN' AS fraud_type,
   CURRENT_TIMESTAMP AS flagged_time
-FROM transactions
+FROM `ssb`.`Real-Time-Fraud-Detection-System`.`fraud_transaction_flattened`
 WHERE CAST(amount AS DOUBLE) > 100000000
 
 UNION ALL
 
--- Rapid repeat device usage
 SELECT
   t1.transaction_id,
   t1.account_id,
   t1.amount,
   'MULTI_DEVICE_TXN' AS fraud_type,
   CURRENT_TIMESTAMP AS flagged_time
-FROM transactions t1
-JOIN transactions t2
+FROM `ssb`.`Real-Time-Fraud-Detection-System`.`fraud_transaction_flattened` t1
+JOIN `ssb`.`Real-Time-Fraud-Detection-System`.`fraud_transaction_flattened` t2
   ON t1.device_id = t2.device_id
-  AND t1.transaction_id != t2.transaction_id
-  AND ABS(t1.timestamp - t2.timestamp) <= 5000
+  AND t1.transaction_id <> t2.transaction_id
+  AND ABS(t1.timestamp - t2.timestamp) <= 5000;
